01be5c3b32d894379e664c4927364e6eb67f7aea
waffledood <itshaikalhere@hotmail.sg>
2022-11-06 09:36:04 +0800

    Create search view function

--- project1-wiki/encyclopedia/views.py
+++ project1-wiki/encyclopedia/views.py
@@ -20,3 +20,24 @@ def entry(request, title):
             "entryContents": entryContents
         })
 
+def search(request):
+    if request.method == "POST":
+        # retrieve the current list of Wiki entries
+        wikiEntries = util.list_entries()
+        # retrieve the query keyword
+        query = request.POST['q']
+
+        # check if the query keyword exactly matches any of the Wiki entries (case insensitive)
+        if query.lower() in (entry.lower() for entry in wikiEntries):
+            return entry(request, query)
+
+        # else find entries that match or contain the query string (case insensitive)
+        entriesContainingQuery = [entry for entry in wikiEntries if query.lower() in entry.lower()]
+        print(f"entriesContainingQuery = {entriesContainingQuery}")
+        # generate the search page with matching Wiki entries
+        return render(request, "encyclopedia/search.html", {
+            "entryContents": entriesContainingQuery
+        })
+    # if the search page was reached through a GET method
+    else:
+        return render(request, "encyclopedia/search.html")
