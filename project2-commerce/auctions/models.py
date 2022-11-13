932b69b8ac8ac571262f13f67c119cc50d3fe29a
waffledood <itshaikalhere@hotmail.sg>
2022-11-13 12:56:36 +0800

    Create Listing model & register it in admin

--- project2-commerce/auctions/models.py
+++ project2-commerce/auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    # image = models.ImageField()
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
