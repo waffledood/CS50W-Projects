62a0cbfc245563a68e12fefa5a3122131dbd20bd
waffledood <itshaikalhere@hotmail.sg>
2022-11-13 13:47:21 +0800

    Include String representation for all models

--- project2-commerce/auctions/models.py
+++ project2-commerce/auctions/models.py
@@ -13,12 +13,21 @@ class Listing(models.Model):
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Listing ({self.id}): {self.name} of ${self.price}"
+
 class Bid(models.Model):
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
     price = models.FloatField()
 
+    def __str__(self):
+        return f"Bid ({self.id}): By {self.bidder.first_name} of ${self.price} for {self.listing.name}"
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     description = models.CharField(max_length=64)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        return f"Comment ({self.id}): By {self.user.first_name} on {self.listing.name}"
