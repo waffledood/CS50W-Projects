a1ee446249e51d62ebfd8ee14f0bba466020e342
waffledood <itshaikalhere@hotmail.sg>
2022-11-14 19:55:44 +0800

    Create NewListingForm

--- project2-commerce/auctions/views.py
+++ project2-commerce/auctions/views.py
@@ -1,10 +1,42 @@
+from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+class NewListingForm(forms.Form):
+    name = forms.CharField(
+        label="Name of Listing:",
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Enter the name for your new listing...',
+                'class': 'form-control w-75 mb-2'
+            }
+        )
+    )
+
+    price = forms.FloatField(
+        label="Price of Listing:",
+        widget=forms.NumberInput(
+            attrs={
+                'class': 'form-control w-75 mb-2',
+                'step': '0.01',
+                'min': '0.0',
+                'max': '100_000.0'
+            }
+        )
+    )
+
+    description = forms.CharField(
+        label="Description of Listing:",
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Enter the description for your new listing...',
+                'class': 'form-control w-75 mb-2'
+            }
+        )
+    )
 
 
 def index(request):
