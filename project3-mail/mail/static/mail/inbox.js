a399845b5ae49979e0c4df0eeb35f4466a99c689
waffledood <itshaikalhere@hotmail.sg>
2022-12-29 09:30:36 +0800

    Display the email-view div in load_mail()

--- project3-mail/mail/static/mail/inbox.js
+++ project3-mail/mail/static/mail/inbox.js
@@ -118,4 +118,8 @@ function load_mailbox(mailbox) {
 }
 
 function load_mail(emailId) {
+  // Show the individual mail and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'block';
 }
