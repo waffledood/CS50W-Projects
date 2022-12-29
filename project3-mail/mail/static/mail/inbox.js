3b0da719dde1019b43643f331bddec9e920b0dcd
waffledood <itshaikalhere@hotmail.sg>
2022-12-29 09:31:05 +0800

    Hide the email-view div in load_mailbox()

--- project3-mail/mail/static/mail/inbox.js
+++ project3-mail/mail/static/mail/inbox.js
@@ -60,6 +60,7 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
