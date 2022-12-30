6221bfad87a5b533b0223633a8e11a2d5bacc6d2
waffledood <itshaikalhere@hotmail.sg>
2022-12-30 08:29:23 +0800

    Create archive button as its own element

--- project3-mail/mail/static/mail/inbox.js
+++ project3-mail/mail/static/mail/inbox.js
@@ -151,6 +151,22 @@ function load_mail(emailId) {
     const utilitiesDiv = document.createElement('div');
     utilitiesDiv.className = `d-flex w-100`;
 
+    // create archive button
+    const archiveBtn = document.createElement('button');
+    archiveBtn.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
+        <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+    `;
+    archiveBtn.type = 'button';
+    archiveBtn.className = 'btn btn-outline-secondary mt-2';
+
+    // add archive button to utilities div
+    utilitiesDiv.append(archiveBtn);
+
+    // create the email list group item
+    let email = document.createElement('div');
+
     // add utilites div to the top of the email element
     email.append(utilitiesDiv);
 
