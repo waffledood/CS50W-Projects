38a9b8dcbfb078639ef57c44e91acd49a1d966dc
waffledood <itshaikalhere@hotmail.sg>
2022-12-31 10:12:09 +0800

    Configure archive button
    The appearance of the archive button differs based on whether the email has been archived or not

--- project3-mail/mail/static/mail/inbox.js
+++ project3-mail/mail/static/mail/inbox.js
@@ -151,9 +151,14 @@ function load_mail(emailId) {
     // create archive button
     const archiveBtn = document.createElement('button');
     archiveBtn.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
-        <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-      </svg>
+      ${emailJSONContent.archived == true ? 
+        `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
+          <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
+        </svg>`
+      : `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
+          <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+        </svg>` 
+      }
     `;
     archiveBtn.type = 'button';
     archiveBtn.className = 'btn btn-outline-secondary mt-2';
@@ -162,7 +167,7 @@ function load_mail(emailId) {
       fetch(`/emails/${emailJSONContent.id}`, {
         method: 'PUT',
         body: JSON.stringify({
-            archived: true
+            archived: `${emailJSONContent.archived == true ? false : true }`
         })
       });
       // load the specified mail
@@ -171,7 +176,7 @@ function load_mail(emailId) {
 
     // add tooltip to archive button
     archiveBtn.setAttribute('data-bs-toggle', 'tooltip');
-    archiveBtn.setAttribute('data-bs-title', 'Archive');
+    archiveBtn.setAttribute('data-bs-title', `${emailJSONContent.archived == true ? 'Unarchive' : 'Archive'}`);
 
     // add archive button to utilities div
     utilitiesDiv.append(archiveBtn);
