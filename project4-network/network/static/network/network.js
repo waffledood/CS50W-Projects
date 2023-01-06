49f78a2838e8d7aaede2dcb2b76d51c06caa535a
waffledood <itshaikalhere@hotmail.sg>
2023-01-06 09:29:09 +0800

    Initialize functions when the DOM is loaded

--- project4-network/network/static/network/network.js
+++ project4-network/network/static/network/network.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    // use buttons to toggle between views
+    document.querySelector('#following').addEventListener('click', () => loadPostsFollowing());
 
+    // create "New Post" functionality
+    document.querySelector('#create-view').addEventListener('click', () => createPost());
+
+    // load the "All Posts" section by default
+    loadAllPosts();
 
 });
 
