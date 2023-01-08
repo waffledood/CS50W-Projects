637d32d7ce1de0bdff8666c73cd4f469e6f9f9c6
waffledood <itshaikalhere@hotmail.sg>
2023-01-08 12:49:13 +0800

    Add Tweet model

--- project4-network/network/models.py
+++ project4-network/network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tweets")
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Tweet ({self.id}): { self.content[:10] + '...' if len(self.content) >= 10 else self.content } by {self.user}"
