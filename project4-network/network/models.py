d9eaca0641bc45977d90289fcb6671f3375aec18
waffledood <itshaikalhere@hotmail.sg>
2023-01-14 20:06:15 +0800

    Define str function for User model

--- project4-network/network/models.py
+++ project4-network/network/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+    def __str__(self):
+        return f"User ({self.id}): {self.username}"
+
 class Follower(models.Model):
     # user is following userFollowing
     user = models.IntegerField()
