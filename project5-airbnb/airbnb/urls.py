f2bbf977b78fc6407b59772d2a3cec81c8ffa9b8
waffledood <itshaikalhere@hotmail.sg>
2023-03-19 15:57:43 +0800

    Define listing view

--- project5-airbnb/airbnb/urls.py
+++ project5-airbnb/airbnb/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name="index"),
+
+    # API Routes
+    path("listing/<int:id>", views.listing, name="listing"),
 ]
--- project5-airbnb/airbnb/views.py
+++ project5-airbnb/airbnb/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+
+from .models import Listing
 
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def listing(request, id):
+    #  Retrieve Listing with specified id
+    listing = Listing.objects.get(id=id)
+
+    return JsonResponse([listing.serialize()], safe=False)
+
