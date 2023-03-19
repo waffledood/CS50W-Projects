f35e79b73d5ec99cf3ca51dcafb9aa5b4d7ae638
waffledood <itshaikalhere@hotmail.sg>
2023-03-19 13:09:06 +0800

    Register User, Listing & Booking models

--- project5-airbnb/airbnb/admin.py
+++ project5-airbnb/airbnb/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Listing, Booking
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Booking)
