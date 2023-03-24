906517c5e7c55dff5d22fccf1691f4aca4865d64
waffledood <itshaikalhere@hotmail.sg>
2023-03-24 22:00:33 +0800

    Fix incorrect _ argument in ValueError

--- project5-airbnb/backend/airbnb/models.py
+++ project5-airbnb/backend/airbnb/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 from django.utils import timezone
 
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         """
@@ -24,10 +25,11 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
+            raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
+            raise ValueError("Superuser must have is_superuser=True.")
         return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
@@ -40,14 +42,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [] 
+    REQUIRED_FIELDS = []
     # REQUIRED_FIELDS = a list of fields that will be prompted for when creating user via createsuperuser command
 
     objects = UserManager()
 
     def __str__(self):
         return f"User ({self.id}): {self.first_name} {self.last_name}, {self.email}"
-    
+
     def serialize(self):
         return {
             "id": self.id,
@@ -61,14 +63,16 @@ class User(AbstractBaseUser, PermissionsMixin):
             "date_joined": self.date_joined
         }
 
+
 class Listing(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     rating = models.DecimalField(max_digits=3, decimal_places=2)
     price_nightly = models.DecimalField(max_digits=5, decimal_places=0)
-    
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
-    
+
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="listings")
+
     def __str__(self):
         return f"Listing ({self.id}): {self.name}, {self.rating} stars, ${self.price_nightly}"
 
@@ -82,16 +86,19 @@ class Listing(models.Model):
             "owner_id": self.owner.id
         }
 
+
 class Booking(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listings")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="bookings")
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="listings")
 
     def __str__(self):
         return f"Booking ({self.id}): {self.start_date} -- {self.end_date} by User {self.user.id} for Listing {self.listing.id}"
-    
+
     def serialize(self):
         return {
             "id": self.id,
