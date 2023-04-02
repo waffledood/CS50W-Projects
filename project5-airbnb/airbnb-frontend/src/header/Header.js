cc5e8f70afc71b1b741828cf44c370d7e57f91c3
waffledood <itshaikalhere@hotmail.sg>
2023-04-02 13:50:15 +0800

    Import Navbar into Header component

--- project5-airbnb/airbnb-frontend/src/header/Header.js
+++ project5-airbnb/airbnb-frontend/src/header/Header.js
@@ -1,10 +1,12 @@
 import classes from "./Header.module.css";
 import Homepage from "./Homepage";
+import Navbar from "./Navbar";
 
 const Header = (props) => {
   return (
     <header className={`${classes.header}`}>
       <Homepage />
+      <Navbar />
     </header>
   );
 };
