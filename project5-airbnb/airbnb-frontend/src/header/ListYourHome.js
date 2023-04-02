69946bc1d6fec59b9a76be5c6fab1c393ba79361
waffledood <itshaikalhere@hotmail.sg>
2023-04-02 13:46:02 +0800

    Define ListYourHome component

---
+++ project5-airbnb/airbnb-frontend/src/header/ListYourHome.js
@@ -0,0 +1,7 @@
+import classes from "./ListYourHome.module.css";
+
+const ListYourHome = (props) => {
+  return <div className={`${classes.div} default-font`}>Airbnb your home</div>;
+};
+
+export default ListYourHome;
