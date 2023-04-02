14eb9b50a7e89f721cd93a77f9b2cf3a71ab0e6f
waffledood <itshaikalhere@hotmail.sg>
2023-04-02 11:13:25 +0800

    Import global-styles.css to index.js

--- project5-airbnb/airbnb-frontend/src/index.js
+++ project5-airbnb/airbnb-frontend/src/index.js
@@ -4,5 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import "./global-styles.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
