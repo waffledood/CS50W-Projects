871778f73be7e6bae5505eb46ce5d054541f471a
waffledood <itshaikalhere@hotmail.sg>
2023-04-08 12:04:16 +0800

    Define Backdrop component in ListYourHomeForm

--- project5-airbnb/airbnb-frontend/src/form/ListYourHomeForm.js
+++ project5-airbnb/airbnb-frontend/src/form/ListYourHomeForm.js
@@ -1,9 +1,20 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import classes from "./ListYourHomeForm.module.css";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onExit}></div>;
+};
 const ListYourHomeForm = (props) => {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onExit={props.onExit} />,
+        document.getElementById("backdrop-root")
+      )}
+    </React.Fragment>
+  );
 };
 
 export default ListYourHomeForm;
