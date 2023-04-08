afac39fb88edd746e4f93e2f90aaaa208bf84f85
waffledood <itshaikalhere@hotmail.sg>
2023-04-08 12:02:18 +0800

    Create ListYourHomeForm component

---
+++ project5-airbnb/airbnb-frontend/src/form/ListYourHomeForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./ListYourHomeForm.module.css";
+
+const ListYourHomeForm = (props) => {
+  return <div></div>;
+};
+
+export default ListYourHomeForm;
