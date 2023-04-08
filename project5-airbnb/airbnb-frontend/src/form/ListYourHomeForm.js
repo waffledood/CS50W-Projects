a137c7101aeda57f43b6c01c94d37197ce2b2205
waffledood <itshaikalhere@hotmail.sg>
2023-04-08 12:04:30 +0800

    Define Modal component in ListYourHomeForm

--- project5-airbnb/airbnb-frontend/src/form/ListYourHomeForm.js
+++ project5-airbnb/airbnb-frontend/src/form/ListYourHomeForm.js
@@ -6,12 +6,38 @@ import classes from "./ListYourHomeForm.module.css";
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.onExit}></div>;
 };
+
+const ModalOverlay = (props) => {
+  const formSubmitHandler = (event) => {
+    // stop form submission
+    event.preventDefault();
+
+    // close the modal overlay
+    props.onExit();
+
+    // handle form submission
+  };
+
+  return (
+    <div className={classes.modal}>
+      <form action="#" onSubmit={formSubmitHandler}>
+        sample form
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
 const ListYourHomeForm = (props) => {
   return (
     <React.Fragment>
       {ReactDOM.createPortal(
         <Backdrop onExit={props.onExit} />,
         document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay onExit={props.onExit} />,
+        document.getElementById("overlay-root")
       )}
     </React.Fragment>
   );
