24c73933cef9b95b6a4d18377634f3d8a3411143
waffledood <itshaikalhere@hotmail.sg>
2023-04-08 12:05:04 +0800

    Include ListYourHomeForm component in ListYourHome

--- project5-airbnb/airbnb-frontend/src/header/ListYourHome.js
+++ project5-airbnb/airbnb-frontend/src/header/ListYourHome.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 import classes from "./ListYourHome.module.css";
+import ListYourHomeForm from "../form/ListYourHomeForm";
 
 const ListYourHome = (props) => {
+  const [listYourHomeClick, setListYourHomeClick] = useState(false);
+
+  const listYourHomeActiveHandler = (props) => {
+    setListYourHomeClick(true);
+  };
+
+  const listYourHomeInactiveHandler = (props) => {
+    setListYourHomeClick(false);
+  };
+
   return (
     <div className={`${classes.div} default-font`}>
-      <a href="/" className={`${classes.a} link`}>
+      {listYourHomeClick && (
+        <ListYourHomeForm onExit={listYourHomeInactiveHandler} />
+      )}
+      <a onClick={listYourHomeActiveHandler} className={`${classes.a} link`}>
         Airbnb your home
       </a>
     </div>
