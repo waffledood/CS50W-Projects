7c6b39b1bb64906e346af2d743ef60e52b3834bb
waffledood <itshaikalhere@hotmail.sg>
2023-06-18 09:07:55 +0800

    Remove ListYourHomeForm from ListYourHome component

--- project5-airbnb/airbnb-frontend/src/pages/createlisting/ListYourHome.js
+++ project5-airbnb/airbnb-frontend/src/pages/createlisting/ListYourHome.js
@@ -1,27 +1,11 @@
 import { useState } from "react";
 
 import classes from "./ListYourHome.module.css";
-import ListYourHomeForm from "../../form/ListYourHomeForm";
 
 const ListYourHome = (props) => {
-  const [listYourHomeClick, setListYourHomeClick] = useState(false);
-
-  const listYourHomeActiveHandler = (props) => {
-    setListYourHomeClick(true);
-  };
-
-  const listYourHomeInactiveHandler = (props) => {
-    setListYourHomeClick(false);
-  };
-
   return (
     <div className={`${classes.div} default-font`}>
-      {listYourHomeClick && (
-        <ListYourHomeForm onExit={listYourHomeInactiveHandler} />
-      )}
-      <a onClick={listYourHomeActiveHandler} className={`${classes.a} link`}>
-        Airbnb your home
-      </a>
+      <a className={`${classes.a} link`}>Airbnb your home</a>
     </div>
   );
 };
