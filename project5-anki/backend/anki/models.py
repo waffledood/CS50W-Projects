e8de3fde100c0c5e2d08866280f912babf87ec54
waffledood <itshaikalhere@hotmail.sg>
2023-08-14 00:05:41 +0800

    Define Collection model

--- project5-anki/backend/anki/models.py
+++ project5-anki/backend/anki/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Collection(models.Model):
+    user_id = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="collections")
+    name = models.CharField(max_length=64)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
 class Card(models.Model):
     collection_id = models.ForeignKey(
         to=Collection, on_delete=models.CASCADE, related_name="cards")
