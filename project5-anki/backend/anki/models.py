5814e49a550b06c52ec3799407c5be5c67e25ba1
waffledood <itshaikalhere@hotmail.sg>
2023-08-14 00:06:29 +0800

    Define custom User model

--- project5-anki/backend/anki/models.py
+++ project5-anki/backend/anki/models.py
@@ -1,4 +1,23 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+
+from .managers import CustomUserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=64, unique=True)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = [username, email]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.username}, {self.email}"
 
 
 class Collection(models.Model):
