dbc39cc7a37f216522b0fb320987ed6fff895ff6
waffledood <itshaikalhere@hotmail.sg>
2023-08-15 22:38:38 +0800

    Remove unused _ function in managers.py

--- project5-anki/backend/anki/managers.py
+++ project5-anki/backend/anki/managers.py
@@ -29,7 +29,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
+            raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
+            raise ValueError("Superuser must have is_superuser=True.")
         return self.create_user(username, email, password, **extra_fields)
