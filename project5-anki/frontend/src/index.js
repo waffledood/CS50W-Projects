83b00a23f9d30cec0cfe1f21fea5b9a2aee85cb9
waffledood <itshaikalhere@hotmail.sg>
2023-09-16 10:18:21 +0800

    Abstract out Navbar to layout HTML template

--- project5-anki/frontend/src/index.js
+++ project5-anki/frontend/src/index.js
@@ -1,27 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-
 import Collection from "./collection/Collection";
 
 const App = () => {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Anki</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#profile">Profile</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
       <Collection />
     </div>
   );
