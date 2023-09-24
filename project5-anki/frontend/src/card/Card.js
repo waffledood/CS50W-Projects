9aaa8a460bf4253cec22e77898d7387cffcbbbf0
waffledood <itshaikalhere@hotmail.sg>
2023-09-24 20:25:54 +0800

    Refactor Card component to accept a listId prop

--- project5-anki/frontend/src/card/Card.js
+++ project5-anki/frontend/src/card/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
   return (
     <tr key={props.id}>
-      <td>{props.cards.length + 1}</td>
+      <td>{props.listId}</td>
       <td>{props.question}</td>
       <td>{props.answer}</td>
       <td>{props.button}</td>
